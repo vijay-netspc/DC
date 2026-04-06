59d3dd1ff82bd0eed379f67807c252fcb60aa99c
vijay-netspc <vijayaram.networking@gmail.com>
2026-04-06 20:16:36 +0530

    Update client.c

--- socket/client.c
+++ socket/client.c
@@ -1,41 +1,39 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <unistd.h> 
-#include <arpa/inet.h> 
-#define PORT 8080 
-int main() { 
- 
- int server_fd, new_socket; struct sockaddr_in address; 
- int addrlen = sizeof(address); char buffer[1024] = {0}; 
- char *message = "Hello from Server"; 
- server_fd = socket(AF_INET, SOCK_STREAM, 0); 
- 
- if (server_fd == -1) { 
-     perror("Socket failed"); 
-     exit(EXIT_FAILURE);} 
- 
- address.sin_family = AF_INET; 
- address.sin_addr.s_addr = INADDR_ANY; 
- address.sin_port = htons(PORT); 
- if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) { 
-     perror("Bind failed"); 
-     exit(EXIT_FAILURE); } 
- if (listen(server_fd, 3) < 0) { 
-     perror("Listen failed"); 
-     exit(EXIT_FAILURE); } 
- 
- printf("Server listening on port %d...\n", PORT); 
- new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen); 
- if (new_socket < 0) { 
-     perror("Accept failed"); 
-     exit(EXIT_FAILURE); } 
- 
- read(new_socket, buffer, sizeof(buffer)); 
- printf("Client says: %s\n", buffer); 
- send(new_socket, message, strlen(message), 0); 
- printf("Message sent to client\n"); 
- close(new_socket); 
- close(server_fd); 
- return 0; 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 5000
+
+int main() {
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[1024];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        exit(1);
+    }
+
+    printf("Connected to server\n");
+
+    while (1) {
+        printf("Client: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        send(sock, buffer, strlen(buffer), 0);
+
+        memset(buffer, 0, sizeof(buffer));
+        recv(sock, buffer, sizeof(buffer), 0);
+        printf("Server: %s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
 }
